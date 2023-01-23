a31ecfcff5803c1c2a201099f5ef7f5727ab1063
Adrian WEAVER <aweaver@student.42.fr>
2023-01-23 16:33:06 +0100

    ADD MAIN.CPP && CONTAINERS.HPP:
    Adding a define container.hpp also updating main to comply with
    these additions.

---
+++ ft_containers.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_containers.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 14:22:48 by aweaver           #+#    #+#             */
+/*   Updated: 2023/01/23 14:23:41 by aweaver          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CONTAINERS
+# define FT_CONTAINERS
+
+# ifndef CUSTOM_SCOPE
+#  define CUSTOM_SCOPE 1
+# endif
+#endif
--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
 #include <deque>
-#if 1 //CREATE A REAL STL EXAMPLE
+#if CUSTOMSCOPE == 0 //CREATE A REAL STL EXAMPLE
 	#include <map>
 	#include <stack>
 	#include <vector>
 	namespace ft = std;
 #else
-	#include <map.hpp>
-	#include <stack.hpp>
-	#include <vector.hpp>
+	#include "map.hpp"
+	#include "stack.hpp"
+	#include "vector.hpp"
+	#include "ft_containers.hpp"
 #endif
 
 #include <stdlib.h>
